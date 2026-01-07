38d82a31cb1ac9172d60d4b71bcb8596a2282f54
Yorn90104 <bingxiao526@gmail.com>
2026-01-07 22:39:05 +0800

    調整 Herosection 組件，註解掉預約連結區塊；新增全域按鈕樣式與互動效果

--- src/components/index/Herosection.tsx
+++ src/components/index/Herosection.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 export const Herosection = () => {
   return (
     <section className="w-full h-screen">
-      <div className="bg-black w-full h-full">
+      {/* <div className="bg-black w-full h-full">
         <div>
           <Link href={"/booking"}>前往預約</Link>
         </div>
-      </div>
+      </div> */}
     </section>
   );
 };
