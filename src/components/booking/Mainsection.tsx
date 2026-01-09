aae28698aeef701529ad4ad6f572c01ad4a0cacb
Yorn90104 <bingxiao526@gmail.com>
2026-01-09 13:44:46 +0800

    調整 Mainsection 組件以使用 booking 物件，優化標籤切換邏輯並顯示當前步驟資料

--- src/components/booking/Mainsection.tsx
+++ src/components/booking/Mainsection.tsx
@@ -4,32 +4,37 @@ import { LocationsDiv } from "./LocationsDiv";
 import { cn } from "@/utils/className";
 
 export const Mainsection = () => {
-  const { currStep, setCurrStep, getStepIndex, setBookingData } = useBooking();
+  const booking = useBooking();
 
   return (
     <section>
       <div className="container flex flex-col py-12 md:py-20">
         {/* 標籤切換欄 */}
         <div className="w-full flex items-center gap-6 overflow-x-auto pb-4 mb-8 border-b border-(--border)">
-          {bookingSteps.map((step, index) => (
-            <button
-              key={step.value}
-              className={cn(
-                `w-full text-(--muted) p-2 whitespace-nowrap font-medium`,
-                {
-                  "text-(--primary)": currStep === step.value,
-                }
-              )}
-              onClick={() => {
-                if (getStepIndex(currStep) > index) {
-                  setCurrStep(step.value);
-                  setBookingData(step.value, undefined);
-                }
-              }}
-            >
-              {step.label}
-            </button>
-          ))}
+          {bookingSteps.map((step, index) => {
+            const data = booking.data[step.value];
+            return (
+              <button
+                key={step.value}
+                className={cn(
+                  `w-full text-(--muted) whitespace-nowrap font-medium`,
+                  "flex flex-col justify-center items-center p-2",
+                  {
+                    "text-(--primary)": booking.currStep === step.value,
+                  }
+                )}
+                onClick={() => {
+                  if (booking.getStepIndex(booking.currStep) > index) {
+                    booking.setCurrStep(step.value);
+                    booking.setBookingData(step.value, undefined);
+                  }
+                }}
+              >
+                <span className="text-lg md:text-xl">{step.label}</span>
+                <span className="text-sm mt-1">{data ? data : "未選擇"}</span>
+              </button>
+            );
+          })}
         </div>
         <LocationsDiv />
       </div>
