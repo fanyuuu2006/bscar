49df6c05f1c5e306ba46dde6d402f7845c115b10
Yorn90104 <bingxiao526@gmail.com>
2026-01-30 18:48:13 +0800

    新增 Dashboard 頁面，包含 MainSection 元件以顯示預訂資訊

---
+++ src/app/admin/dashboard/booking/page.tsx
@@ -0,0 +1,5 @@
+import { MainSection } from "@/components/admin/dashboard/booking/MainSection";
+
+export default function Dashboard() {
+  return <MainSection />;
+}
---
+++ src/components/admin/dashboard/booking/MainSection.tsx
@@ -0,0 +1,5 @@
+export const MainSection = () => {
+  return (
+    <section className="flex h-full w-full items-center justify-center p-4"></section>
+  );
+};
