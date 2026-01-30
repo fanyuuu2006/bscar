6ac445bcc3cf9c43d8c671d0a0d01899fe1cb8eb
Yorn90104 <bingxiao526@gmail.com>
2026-01-30 15:09:53 +0800

    新增 Layout 元件以包裹管理頁面，並引入 AdminProvider 以支援管理員功能

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,19 @@
+import { AdminProvider } from "@/contexts/AdminContext";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "後台",
+  description: "後台頁面",
+};
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <AdminProvider>
+        {children}
+    </AdminProvider>
+  );
+}
