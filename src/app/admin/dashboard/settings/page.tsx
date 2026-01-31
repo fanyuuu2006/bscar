3022f18c09e2422e9a4aad16ddeedc7500051b42
Yorn90104 <bingxiao526@gmail.com>
2026-01-31 16:42:17 +0800

    新增 MainSection 元件，作為預約設定頁面的主要區域

---
+++ src/app/admin/dashboard/settings/page.tsx
@@ -0,0 +1,5 @@
+import { MainSection } from "@/components/admin/dashboard/settings/MainSection";
+
+export default function Booking() {
+  return <MainSection />;
+}
---
+++ src/components/admin/dashboard/settings/MainSection.tsx
@@ -0,0 +1,7 @@
+export const MainSection = () => {
+  return (
+    <section className="flex h-full w-full flex-col gap-4 p-4">
+        <h2 className="text-2xl font-bold">設定</h2>
+    </section>
+  );
+};
