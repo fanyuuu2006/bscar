2e140123436d4ff302e62f261d3f544862da02bc
Yorn90104 <bingxiao526@gmail.com>
2026-01-31 14:42:55 +0800

    調整 MainSection 元件以改善預約資訊顯示，新增預約編號、服務和預約時間的格式化顯示

--- src/components/admin/dashboard/booking/[id]/MainSection.tsx
+++ src/components/admin/dashboard/booking/[id]/MainSection.tsx
@@ -1,5 +1,6 @@
 import { SupabaseBooking, SupabaseLocation, SupabaseService } from "@/types";
 import { cn } from "@/utils/className";
+import { formatDate } from "@/utils/date";
 import { OverrideProps } from "fanyucomponents";
 
 type MainSectionProps = OverrideProps<
@@ -23,13 +24,24 @@ export const MainSection = ({
       <div className="w-full flex flex-col p-4 gap-4">
         <h2 className="text-2xl font-bold">編輯預約</h2>
 
+        {/* 預約資訊 */}
         <div className="card p-4 rounded-xl">
           <h3 className="text-lg font-bold">預約資訊</h3>
 
           <div className="mt-2 flex flex-col gap-2">
-            <p>
-              <span className="font-medium">預約ID:</span> {booking.id}
-            </p>
+            <div className="flex flex-col">
+                <span className="font-medium">預約編號</span>
+                <span>{booking.id}</span>
+
+            </div>
+            <div className="flex flex-col">
+                <span className="font-medium">服務</span>
+                <span>{service ? service.name : booking.service_id}</span>
+            </div>
+            <div className="flex flex-col">
+                <span className="font-medium">預約時間</span>
+                <span>{formatDate("YYYY/MM/DD hh:mm A", booking.booking_time)}</span>
+            </div>
           </div>
         </div>
       </div>
