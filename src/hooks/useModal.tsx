f22538926b0776323a4872fd1d61de70317ca597
Yorn90104 <bingxiao526@gmail.com>
2026-02-23 12:12:11 +0800

    修正 useModal 中的 onClose 回調，移除不必要的非空斷言

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -33,7 +33,7 @@ export const useModal = ({
     if (dialog.open) {
       dialog.close();
       setIsModalOpen(false);
-      onClose?.(dialog!);
+      onClose?.(dialog);
     }
   }, [onClose]);
 
